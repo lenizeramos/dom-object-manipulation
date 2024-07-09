0bea66785e06e309b059f6623f76f0fe42ec3897
lenizeramos <lenizemr@gmail.com>
2024-07-09 08:54:29 -0700

    exercise completed

--- modal.js
+++ modal.js
@@ -1,9 +1,24 @@
-console.log("here");
+document.addEventListener("DOMContentLoaded", () => {
+  // select button element
+  const openModal = document.getElementById("openModal");
+  const modal = document.getElementById("modal");
+  let modalTimeout =
+    // add event listener on click to button
+    openModal.addEventListener("click", () => {
+      // remove class invisible from div modal
+      modal.classList.remove("invisible");
+      // add timeout
+      modalTimeout = setTimeout(() => {
+        modal.classList.add("invisible");
+      }, 5000);
+    });
 
-// select button element
-
-// add event listener on click to button
-// remove class invisible from div modal
-
-// add event listener on key press Escape
-// add class invisible to div modal
+  // add event listener on key press Escape
+  document.addEventListener("keydown", (event) => {
+    // add class invisible to div modal
+    if (event.key === "Escape") {
+      modal.classList.add("invisible");
+      clearTimeout(modalTimeout);
+    }
+  });
+});
